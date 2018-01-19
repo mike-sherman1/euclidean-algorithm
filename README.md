5e2d03a13e30abda6babff8e5764df270b546901
Mike Sherman <mikesherman315@gmail.com>
2018-01-19 11:30:35 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# euclidean-algorithm
+This program computes the greatest common divisor of two integers using the
+Extended Euclidean Algorithm. The program runs until the user enters 1.
--- extended_euclidean_algorithm.cpp
+++ extended_euclidean_algorithm.cpp
@@ -1,3 +1,8 @@
+/*
+This program computes the greatest common divisor of two integers using the
+Extended Euclidean Algorithm. The program runs until the user enters 1.
+*/
+
 #include <iostream>
 #include <string>
 
